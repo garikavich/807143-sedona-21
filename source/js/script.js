5d83c4cd01309904060dcbd653277332a68d9ace
Garkusha Oleg <38359645+garikavich@users.noreply.github.com>
2021-01-23 21:30:40 +0200

    Automotizaciya

--- source/js/script.js
+++ source/js/script.js
@@ -4,11 +4,11 @@ var navToggle = document.querySelector('.page-header__button-toggle');
 navMain.classList.remove('page-header__nav--nojs');
 
 navToggle.addEventListener('click', function () {
-	if (navMain.classList.contains('page-header__nav--closed')) {
-		navMain.classList.remove('page-header__nav--closed');
-		navMain.classList.add('page-header__nav--opened');
-	} else {
-		navMain.classList.add('page-header__nav--closed');
-		navMain.classList.remove('page-header__nav--opened');
-	}
+  if (navMain.classList.contains('page-header__nav--closed')) {
+    navMain.classList.remove('page-header__nav--closed');
+    navMain.classList.add('page-header__nav--opened');
+  } else {
+    navMain.classList.add('page-header__nav--closed');
+    navMain.classList.remove('page-header__nav--opened');
+  }
 });
